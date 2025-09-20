7bcaa9cf85c266829630dab3ed45389f69ad266c
Atul Pandey <pandeyatulkumar94@gmail.com>
2025-09-20 14:48:08 +0530

    Refactor database connection to use DB_PASSWORD from constants

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,10 +1,11 @@
 import { Sequelize } from "sequelize";
+import { DB_PASSWORD } from "../../constants.js";
 
 // Use environment variables for host, user, password, and database name
 const sequelize = new Sequelize(
   "mojo-DB",    // database name
   "root",       // username
-  "Atul#2002",       // password
+  DB_PASSWORD,       // password
   {
     host: process.env.DB_HOST || "localhost",
     dialect: "mysql",
