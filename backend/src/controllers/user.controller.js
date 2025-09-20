30e7def224e8b42c56f6b10eee0efb8eae42702c
Atul Pandey <pandeyatulkumar94@gmail.com>
2025-09-20 19:19:12 +0530

    added getUserById route

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -121,3 +121,53 @@ export const updateUser = async (req, res) => {
     res.status(500).json({ message: "Server error", error });
   }
 };
+
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.findAll({
+      attributes: { exclude: ["password"] }, // Exclude password from response
+    });
+
+    res.status(200).json({
+      success: true,
+      data: users,
+    });
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    res.status(500).json({
+      success: false,
+      message: "Server error",
+      error,
+    });
+  }
+};
+
+// Get User by id
+export const getUserById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await User.findByPk(id, {
+      attributes: { exclude: ["password"] }, // Exclude password
+    });
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: `User with ID ${id} not found`,
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: user,
+    });
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    res.status(500).json({
+      success: false,
+      message: "Server error",
+      error,
+    });
+  }
+};
