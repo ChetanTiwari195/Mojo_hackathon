b6006f19b25ec3c4dcd4d4eea3801a962611db41
ChetanTiwari195 <chetan56tiwari@gmail.com>
2025-09-20 22:53:16 +0530

    p

--- Frontend/src/components/ui/navbar.tsx
+++ Frontend/src/components/ui/navbar.tsx
@@ -64,7 +64,7 @@ function DashboardNavbar() {
       {/* Desktop Navigation */}
       <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
         <a
-          href="#"
+          href="/"
           className="flex items-center gap-2 text-lg font-semibold md:text-base"
         >
           <Package2 className="h-6 w-6" />
